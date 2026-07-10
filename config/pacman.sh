28e2df8b7bf1dcdcb5872ead3a0c3cdbbafd0a83
lipengbo <lipengbo@msn.com>
2026-07-10 17:13:47 +0800

    change git label

--- config/pacman.sh
+++ config/pacman.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
-pacman -S --needed mingw-w64-ucrt-x86_64-clang #21.1.8-4
-pacman -S --needed mingw-w64-ucrt-x86_64-cppcheck #2.19.1-1
-pacman -S --needed mingw-w64-ucrt-x86_64-doxygen #1.16.1-1
-pacman -S --needed mingw-w64-ucrt-x86_64-fzf #0.67.0-1
-pacman -S --needed mingw-w64-ucrt-x86_64-gcc #15.2.0-11
-pacman -S --needed mingw-w64-ucrt-x86_64-gdb #17.1-3
-pacman -S --needed mingw-w64-ucrt-x86_64-graphviz #14.1.1-3
-pacman -S --needed mingw-w64-ucrt-x86_64-libwinpthread #13.0.0.r505.g7d006b2ea-1
-pacman -S --needed mingw-w64-ucrt-x86_64-make #4.4.1-4
-pacman -S --needed mingw-w64-ucrt-x86_64-openblas #0.3.31-1
-pacman -S --needed mingw-w64-ucrt-x86_64-python-matplotlib #3.10.8-2
-pacman -S --needed mingw-w64-ucrt-x86_64-python-numpy #2.4.1-2
-pacman -S --needed mingw-w64-ucrt-x86_64-python-pandas #2.3.3-2
-pacman -S --needed mingw-w64-ucrt-x86_64-python-scipy #1.17.0-2
-pacman -S --needed mingw-w64-ucrt-x86_64-qhull #2020.2-3
-pacman -S --needed mingw-w64-ucrt-x86_64-ripgrep #15.1.0-1
-pacman -S --needed mingw-w64-ucrt-x86_64-tools #13.0.0.r505.g7d006b2ea-1
-pacman -S --needed mingw-w64-ucrt-x86_64-winpthreads #13.0.0.r505.g7d006b2ea-1
-pacman -S --needed mingw-w64-ucrt-x86_64-winstorecompat #13.0.0.r505.g7d006b2ea-1
-pacman -S --needed mingw-w64-ucrt-x86_64-python-black mingw-w64-ucrt-x86_64-python-ipython
-pacman -S --needed mingw-w64-ucrt-x86_64-ripgrep mingw-w64-ucrt-x86_64-cmake mingw-w64-ucrt-x86_64-bat mingw-w64-ucrt-x86_64-ctags 
-pacman -S --needed ncdu neofetch openssh parallel procps-ng rsync tmux tree unzip vim axel p7zip unzip tig git make colordiff
+pacman -S --needed mingw-w64-ucrt-x86_64-clang                              \
+    mingw-w64-ucrt-x86_64-cppcheck mingw-w64-ucrt-x86_64-doxygen            \    
+    mingw-w64-ucrt-x86_64-fzf mingw-w64-ucrt-x86_64-gcc                     \
+    mingw-w64-ucrt-x86_64-gdb mingw-w64-ucrt-x86_64-graphviz                \
+    mingw-w64-ucrt-x86_64-libwinpthread mingw-w64-ucrt-x86_64-make          \
+    mingw-w64-ucrt-x86_64-openblas mingw-w64-ucrt-x86_64-python-matplotlib  \
+    mingw-w64-ucrt-x86_64-python-numpy mingw-w64-ucrt-x86_64-python-pandas  \
+    mingw-w64-ucrt-x86_64-python-scipy mingw-w64-ucrt-x86_64-qhull          \
+    mingw-w64-ucrt-x86_64-ripgrep mingw-w64-ucrt-x86_64-tools               \
+    mingw-w64-ucrt-x86_64-winpthreads mingw-w64-ucrt-x86_64-winstorecompat  \
+    mingw-w64-ucrt-x86_64-python-black mingw-w64-ucrt-x86_64-python-ipython \
+    mingw-w64-ucrt-x86_64-ripgrep mingw-w64-ucrt-x86_64-cmake               \
+    mingw-w64-ucrt-x86_64-bat mingw-w64-ucrt-x86_64-ctags                   \
+    ncdu neofetch openssh parallel procps-ng rsync tmux                     \
+    tree unzip vim axel p7zip unzip tig git make colordiff                  \
