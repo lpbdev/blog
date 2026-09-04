20d260749bf4d28fd15f158662caa359c65ca0b6
lipengbo <lipengbo@msn.com>
2026-09-04 21:13:28 +0800

    date

--- config/pacman.sh
+++ config/pacman.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pacman -S --needed mingw-w64-ucrt-x86_64-clang                              \
-    mingw-w64-ucrt-x86_64-cppcheck mingw-w64-ucrt-x86_64-doxygen            \    
+    mingw-w64-ucrt-x86_64-cppcheck mingw-w64-ucrt-x86_64-doxygen            \
     mingw-w64-ucrt-x86_64-fzf mingw-w64-ucrt-x86_64-gcc                     \
     mingw-w64-ucrt-x86_64-gdb mingw-w64-ucrt-x86_64-graphviz                \
     mingw-w64-ucrt-x86_64-libwinpthread mingw-w64-ucrt-x86_64-make          \
@@ -12,5 +12,7 @@ pacman -S --needed mingw-w64-ucrt-x86_64-clang                              \
     mingw-w64-ucrt-x86_64-python-black mingw-w64-ucrt-x86_64-python-ipython \
     mingw-w64-ucrt-x86_64-ripgrep mingw-w64-ucrt-x86_64-cmake               \
     mingw-w64-ucrt-x86_64-bat mingw-w64-ucrt-x86_64-ctags                   \
+    mingw-w64-ucrt-x86_64-clang-tools-extra \
+    mingw-w64-ucrt-x86_64-global \
     ncdu neofetch openssh parallel procps-ng rsync tmux                     \
     tree unzip vim axel p7zip unzip tig git make colordiff                  \
